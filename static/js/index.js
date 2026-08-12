a11b9ae29a1fc20809daf8a70ce4ab81395126dd
davimissola <davigarciamissola@gmail.com>
2026-08-12 00:29:48 -0300

    mais imagens

--- static/js/index.js
+++ static/js/index.js
@@ -3,9 +3,14 @@ const produtos_grid = document.querySelector('.todosprodutos-grid')
 
 
 todos_produtos = [
-    {id: 1, titulo:'Produto 01', descricao: 'Produto 01 0101010101101', preco: 'R$ 129,99'},
-    {id: 2, titulo:'Produto 02', descricao: 'Produto 02 0101010101101', preco: 'R$ 129,99'},
-    {id: 3, titulo:'Produto 03', descricao: 'Produto 03 0101010101101', preco: 'R$ 129,99'}
+    {id: 1, titulo:'Produto 01', descricao: '--------------------', preco: 'R$ 000,00'},
+    {id: 2, titulo:'Produto 02', descricao: '--------------------', preco: 'R$ 000,00'},
+    {id: 3, titulo:'Produto 03', descricao: '--------------------', preco: 'R$ 000,00'},
+    {id: 4, titulo:'Produto 04', descricao: '--------------------', preco: 'R$ 000,00'},
+    {id: 5, titulo:'Produto 05', descricao: '--------------------', preco: 'R$ 000,00'},
+    {id: 6, titulo:'Produto 06', descricao: '--------------------', preco: 'R$ 000,00'},
+    {id: 7, titulo:'Produto 07', descricao: '--------------------', preco: 'R$ 000,00'},
+    {id: 8, titulo:'Produto 08', descricao: '--------------------', preco: 'R$ 000,00'},
 ]
 
 
